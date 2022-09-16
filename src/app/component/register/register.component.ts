17bc87d4e28940ec84d22833cf52b3d1bc16df49
Maximilien Roussel <ro.maximilien@gmail.com>
2022-09-16 15:39:01 +0200

    add old controle

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder, Validators } from '@angular/forms';
+import { User } from 'src/app/model/user';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  form!:FormGroup;
+  user!:User;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
-  }
+    this.form=this.fb.group({
+      pseudo:this.fb.control('',[Validators.required]),
+      password:this.fb.control('',[Validators.required]),
+      old:this.fb.control('',[Validators.required])
+  })
+}
 
+get pseudo(){
+  return this.form.get('pseudo')
+}
+get password(){
+  return this.form.get('password')
+}
+get old(){
+  return this.form.get('old')
+}
 }
