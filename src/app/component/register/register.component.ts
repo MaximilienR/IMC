b04247af33aaa542212149e07fc38437c512de7b
Maximilien Roussel <ro.maximilien@gmail.com>
2022-09-16 16:45:37 +0200

    correction bug

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { User } from 'src/app/model/user';
 
 @Component({
@@ -8,25 +8,34 @@ import { User } from 'src/app/model/user';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  form!:FormGroup;
-  user!:User;
-  constructor(private fb:FormBuilder) { }
+  form!: FormGroup;
+  user!: User;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.form=this.fb.group({
-      pseudo:this.fb.control('',[Validators.required]),
-      password:this.fb.control('',[Validators.required]),
-      old:this.fb.control('',[Validators.required])
-  })
-}
+    this.form = this.fb.group({
+      pseudo: this.fb.control('', [Validators.required]),
+      password: this.fb.control('', [Validators.required],),
+      old: this.fb.control('', [Validators.required]),
+      cut: this.fb.control('', [Validators.required]),
+      weight:this.fb.control('',[Validators.required])
+    })
+  }
 
-get pseudo(){
-  return this.form.get('pseudo')
-}
-get password(){
-  return this.form.get('password')
-}
-get old(){
-  return this.form.get('old')
+  get pseudo() {
+    return this.form.get('pseudo')
+  }
+  get password() {
+    return this.form.get('password')
+  }
+  get old() {
+    return this.form.get('old')
+  }
+
+  get cut() {
+    return this.form.get('cut')
+  }
+get weight(){
+  return this.form.get('weight')
 }
 }
