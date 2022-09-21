904a01a685a9b1ecab44bf689d2f999339ba5898
Maximilien Roussel <ro.maximilien@gmail.com>
2022-09-21 16:57:49 +0200

    update

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
 
   deconnexion(){
     this.loginService.deconnec();
-    this.route.navigate( [''])
+    this.route.navigate( ['register'])
   }
 
 }
