a5ef39a6d401f9cfbbadc514493624182f54ecd0
Corentin Duriez <corentinduriez22@gmail.com>
2022-09-22 11:11:01 +0200

    Separate express to Angular

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,8 +1,10 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- express/middleware/VerifyPseudo.js
+++ express/middleware/VerifyPseudo.js
@@ -3,12 +3,12 @@ const  User  = require("../config/sequelize");
 checkUpUsername = (req, res, next) => {
   User.findOne({
     where: {
-      username: req.body.name,
+      name: req.body.name,
     },
   }).then((user) => {
     if (user) {
       return res.status(400).send({
-        message: "Username is already in use",
+        message: "name is already in use",
       });
     }
   });
