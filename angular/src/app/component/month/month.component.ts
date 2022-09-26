afba7f224ccf973c254f569b852174ddce6aa8f6
Corentin Duriez <corentinduriez22@gmail.com>
2022-09-26 12:03:07 +0200

    Update to month & trimester

--- angular/src/app/component/month/month.component.ts
+++ angular/src/app/component/month/month.component.ts
@@ -7,11 +7,23 @@ import { ImcService } from 'src/app/service/imc.service';
   styleUrls: ['./month.component.css']
 })
 export class MonthComponent implements OnInit {
-imcData : any
+imcData : any[]
+next : any = 0
+compteur : any = 1
   constructor(private imcService : ImcService) { }
 
   deleteIMC(value:any){
     this.imcData.splice(value,1)
+  }
+
+  previousImc(){
+    this.next = this.next - 5
+    this.compteur -= 1
+  }
+
+  nextImc(){
+    this.next = this.next + 5
+    this.compteur += 1
   }
 
   ngOnInit(): void {
--- angular/src/app/component/trimester/trimester.component.ts
+++ angular/src/app/component/trimester/trimester.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { ImcService } from 'src/app/service/imc.service';
 
 @Component({
   selector: 'app-trimester',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trimester.component.css']
 })
 export class TrimesterComponent implements OnInit {
+imcData : any[]
+color: any
+next : any = 0
+compteur : any = 1
 
-  constructor() { }
+  constructor(private imcService : ImcService) { }
 
   ngOnInit(): void {
-  }
+    this.imcData = this.imcService.getimcData()
+     console.log(this.imcData)
 
-}
+
+
+    }
+
+    previousImc(){
+      this.next = this.next - 5
+      this.compteur -= 1
+    }
+
+    nextImc(){
+      this.next = this.next + 5
+      this.compteur += 1
+    }
+   }
+
+
+
